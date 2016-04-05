fb3e3f3743d52ddba9967119acfb2319b3b8ca4d
Alfredo Monro <fredydeltoro@gmail.com>
2016-04-05 11:28:02 -0500

    pruebas de rendereo con backbone

--- control/static/js/backbone/views/pacientes-app.js
+++ control/static/js/backbone/views/pacientes-app.js
@@ -2,11 +2,32 @@ var view = Backbone.View.extend({
   'events' : {
     'click .btn-expediente' : 'algo'
   },
+  initialize : function () {
+    this.template = _.template('<tr>'+
+      '<td>'+
+        '<%= paciente__expediente %>'+
+      '</td>'+
+      '<td>'+
+        '<%= paciente__nombre %> <%= paciente__apaterno %>'+
+      '</td>'+
+      '<td>'+
+        '<%= fecha_internado %>'+
+      '</td>'+
+      '<td>'+
+        '<%= cama %>'+
+      '</td>'+
+    '</tr>')
+  },
   algo : function (e) {
     e.preventDefault()
-    alert('a huevo')
+    alert('tovia nell')
   },
   el :$('body'),
+  render : function () {
+		var data = this.model.toJSON()
+		var html = this.template(data)
+		this.$el.find('.table-pacients tbody').html(html)
+	},
 
 })
 
--- control/static/js/pacientes.js
+++ control/static/js/pacientes.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   console.log('se cargo bien')
+  collection.each(function (model) {
+		//Agregando Nuevas Vistas
+	var ren = new view({model: model})
+  ren.render()
+  $('.table-pacients tbody').append(ren)
+	})
+
 })
