ff0194491846ba795f5890b3a398c3dc267cbfcf
Alfredo Monroy <fredydeltoro@gmail.com>
2016-04-08 18:55:51 -0500

    optimizando js

--- control/static/js/backbone/views/pacientes-app.js
+++ control/static/js/backbone/views/pacientes-app.js
@@ -1,22 +1,30 @@
 var view = Backbone.View.extend({
   events : {
     'click .btn-expediente' : function (e) {
-      e.preventDefault()
-      this.orderby_({row : 'paciente__expediente'})
+      this.test(e,{row : 'paciente__expediente'})
     },
     'click .btn-nombre' : function (e) {
-      e.preventDefault()
-      this.orderby_({row : 'paciente__nombre'})
+      this.orderby_({row : 'paciente__nombre'},e)
     },
     'click .btn-fecha' : function (e) {
-      e.preventDefault()
-      this.orderby_({row : 'fecha_internado'})
+      this.orderby_({row : 'fecha_internado'},e)
     }
   },
   initialize : function () {
 
   },
-  orderby_ : function (data) {
+  test : function (e, data) {
+    e.preventDefault()
+    $(this).off('click')
+    alert('first click ' + data.row)
+    $(e.currentTarget).click(
+      function() {
+        alert('second click ' + data.row)
+      }
+    )
+  },
+  orderby_ : function (data, e) {
+    e.preventDefault()
     var new_oreder = pacientsView.collection.sortBy(data.row)
     pacientsView.collection.models = new_oreder
     $('.table-pacients tbody tr').remove()
