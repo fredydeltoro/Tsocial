4a676142412966bf1d6e999bdc71369ac656b0ef
Alfredo Monroy <fredydeltoro@gmail.com>
2016-05-03 18:00:08 -0500

    mejorando la vista pacientes y agregando filtro

--- control/static/js/controllers/pacientsController.js
+++ control/static/js/controllers/pacientsController.js
@@ -1,8 +1,9 @@
 
 app_pacientes.controller('pacientsController',['$scope', 'pacients', function ($scope, pacients) {
-  $scope.sortType     = 'fecha_internado'; // set the default sort type
-  $scope.sortReverse  = false;  // set the default sort order
-  $scope.searchPacient   = '';
+  $scope.sortType     = 'fecha_internado' // set the default sort type
+  $scope.sortReverse  = false  // set the default sort order
+  $scope.searchPacient   = ''
+  $scope.filters = ''
   pacients.success(function (data) {
     $scope.pacients = data
   })
