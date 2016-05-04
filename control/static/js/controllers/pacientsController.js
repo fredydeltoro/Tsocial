92fd39e519ad0757455079a4deb916838a2876d9
Alfredo Monro <fredydeltoro@gmail.com>
2016-05-04 01:11:49 -0500

    optimizando js

--- control/static/js/controllers/pacientsController.js
+++ control/static/js/controllers/pacientsController.js
@@ -4,6 +4,11 @@ app_pacientes.controller('pacientsController',['$scope', 'pacients', function ($
   $scope.sortReverse  = false  // set the default sort order
   $scope.searchPacient   = ''
   $scope.filters = ''
+  $scope.order_by = function (e, type) {
+    e.preventDefault()
+    $scope.sortType = type
+    $scope.sortReverse = !$scope.sortReverse
+  }
   pacients.success(function (data) {
     $scope.pacients = data
   })
