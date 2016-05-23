4ca679793287f6a18563613df189a367619b1114
Alfredo Monroy <fredydeltoro@gmail.com>
2016-05-23 17:48:16 -0500

    diseño del formulario para la vista perfil

--- control/static/js/controllers/pacientPerfil.js
+++ control/static/js/controllers/pacientPerfil.js
@@ -1,5 +1,6 @@
 app_paciente.controller('pacientPerfil',['$scope','perfil',
 function ($scope, perfil) {
+  $scope.form = 0
   perfil.getPacient($scope.id).success(function (data) {
     $scope.paciente = data
   })
