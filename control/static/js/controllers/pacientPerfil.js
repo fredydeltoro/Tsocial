527ee6a144296d407e13e348564b8aa68791a8f1
Alfredo Monroy <fredydeltoro@gmail.com>
2016-05-25 17:36:32 -0500

    optimizando el controlador de perfil paciente

--- control/static/js/controllers/pacientPerfil.js
+++ control/static/js/controllers/pacientPerfil.js
@@ -1,23 +1,26 @@
 app_paciente.controller('pacientPerfil',['$scope','perfil',
 function ($scope, perfil) {
-  $scope.form = 0
-  $scope.curp = ''
+  $scope.getPacient = function () {
+    perfil.getPacient($scope.id).success(function (data) {
+      $scope.paciente = data
+      $scope.curp = $scope.paciente.curp
+      $scope.nombre = $scope.paciente.nombre
+    })
+  }
+  $scope.getPacient()
+  $scope.form = false
   $scope.submit = function () {
     data = {
-      'curp': $scope.curp
+      'curp' : $scope.curp,
+      'nombre' : $scope.nombre
     }
     perfil.putPacient($scope.id, data)
     .success(function (response) {
-      perfil.getPacient($scope.id).success(function (data) {
-        $scope.paciente = data
-      })
+      $scope.getPacient()
       console.log(response)
     })
     .error(function (response) {
       console.log(response)
     })
   }
-  perfil.getPacient($scope.id).success(function (data) {
-    $scope.paciente = data
-  })
 }])
