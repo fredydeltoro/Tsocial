8249e7af3f7369066fc35af7c7e12d9b5cdbb4af
Alfredo Monroy <fredydeltoro@gmail.com>
2016-06-07 17:36:29 -0500

    actulización de datos en vista info clinica

--- control/static/js/controllers/pacientInfo.js
+++ control/static/js/controllers/pacientInfo.js
@@ -1,6 +1,33 @@
 app_paciente.controller('pacientInfo',['$scope','info',
 function ($scope, info) {
-  info.getPacient($scope.id).success(function (data) {
-    $scope.info = data
-  })
+  $scope.getInfo = function () {
+    info.getPacient($scope.id).success(function (data) {
+      $scope.info = data
+      $scope.fecha_internado = $scope.info.fecha_internado
+      $scope.diagnostico_ingreso = $scope.info.diagnostico_ingreso
+      $scope.estado_Salud = $scope.info.estado_Salud
+      $scope.servicios = $scope.info.servicios
+      $scope.cama = $scope.info.cama
+    })
+  }
+  $scope.getInfo()
+  $scope.update_info = function (e) {
+    e.preventDefault()
+    data = {
+      'fecha_internado' : $scope.fecha_internado,
+      'diagnostico_ingreso' : $scope.diagnostico_ingreso,
+      'estado_Salud' : $scope.estado_Salud,
+      'servicios' : $scope.servicios,
+      'cama' : $scope.cama
+    }
+    info.putPacient($scope.id, data)
+    .success(function (response) {
+      $scope.getInfo()
+      $scope.form = false
+      console.log(response)
+    })
+    .error(function (response) {
+      console.log(response)
+    })
+  }
 }])
--- control/static/js/services/info.js
+++ control/static/js/services/info.js
@@ -2,12 +2,21 @@ app_paciente.factory('info', ['$http', function($http) {
   var Info = {}
   Info.getPacient = function (id) {
     return $http.get('/api-ts/info/' + id)
-              .success(function(data) {
-                return data;
-              })
-              .error(function(err) {
-                return err;
-              })
+    .success(function(data) {
+      return data;
+    })
+    .error(function(err) {
+      return err;
+    })
+  }
+  Info.putPacient = function (id, data) {
+    return $http.put('/api-ts/info/' +id+'/', data)
+    .success(function (response) {
+        return response.status
+      })
+      .error(function (response) {
+        return response.status
+      })
   }
   return Info
 }])
